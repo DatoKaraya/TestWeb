e78d0ea3569020113957a620f8a768c0c8efc0e4
Dato Karaya <datokaraya@gmail.com>
2019-03-25 10:42:44 +0200

    Create form.js

---
+++ localhost/js/form.js
@@ -0,0 +1,25 @@
+
+// A $( document ).ready() block.
+$(document).ready(function() {
+    $("#upload_form").submit(function( event ) {
+
+        event.preventDefault();
+
+        $.ajax({
+            type: $(this).attr('method'),
+            url: $(this).attr('action'),
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+
+
+        });
+
+
+    });
+
+
+
+
+});
